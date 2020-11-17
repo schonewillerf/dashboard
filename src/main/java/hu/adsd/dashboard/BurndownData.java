6b1500ae9d3b33d3e96f7c2be58be4c00fb9bb93
Jan-Willem van Lunteren <jan-willem.vanlunteren@student.hu.nl>
2020-11-17 15:24:36 +0100

    Installed frontend template Created methods for burndown data Changed name of IndicatorRepository

---
+++ src/main/java/hu/adsd/dashboard/BurndownData.java
@@ -0,0 +1,40 @@
+package hu.adsd.dashboard;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+public class BurndownData {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private Date date;
+    private int quantity;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
--- src/main/java/hu/adsd/dashboard/Repository.java
+++
@@ -1,7 +0,0 @@
-package hu.adsd.dashboard;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Repository extends JpaRepository<Indicator, Integer> {
-
-}
