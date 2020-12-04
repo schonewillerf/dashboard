707ddab9465157c2e7e2220ede799be6b0f033da
Raymond Schonewille <schonewillerf@gmail.com>
2020-12-04 11:49:51 +0100

    :zap: Seperate sentiment data from projects

--- src/main/java/hu/adsd/dashboard/sentiment/SentimentData.java
+++ src/main/java/hu/adsd/dashboard/sentiment/SentimentData.java
@@ -1,50 +1,58 @@
 package hu.adsd.dashboard.sentiment;
 
-import javax.persistence.*;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.time.LocalDate;
-import java.util.Date;
 
 @Entity
-public class SentimentData {
+public class SentimentData
+{
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue( strategy = GenerationType.IDENTITY )
     private int id;
-    private Date date;
+    private LocalDate date;
     private int value;
 
-    public SentimentData() {
+    public SentimentData()
+    {
     }
 
-    public SentimentData(int value) throws ParseException {
-        String currentdate = String.valueOf(LocalDate.now());
-        Date formattedDate = new SimpleDateFormat("yyyy-MM-dd").parse(currentdate);
+    public SentimentData( int value )
+    {
+        LocalDate formattedDate = LocalDate.now();
         this.date = formattedDate;
         this.value = value;
     }
 
-    public int getId() {
+    public int getId()
+    {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId( int id )
+    {
         this.id = id;
     }
 
-    public Date getDate() {
+    public LocalDate getDate()
+    {
         return date;
     }
 
-    public void setDate(Date date) {
+    public void setDate( LocalDate date )
+    {
         this.date = date;
     }
 
-    public int getValue() {
+    public int getValue()
+    {
         return value;
     }
 
-    public void setValue(int value) {
+    public void setValue( int value )
+    {
         this.value = value;
     }
 }
--- src/main/java/hu/adsd/dashboard/sentiment/SentimentDataRepository.java
+++ src/main/java/hu/adsd/dashboard/sentiment/SentimentDataRepository.java
@@ -3,11 +3,14 @@ package hu.adsd.dashboard.sentiment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import java.util.Date;
+import java.time.LocalDate;
 import java.util.List;
 
 public interface SentimentDataRepository extends JpaRepository<SentimentData, Integer> {
 
     @Query("SELECT SDE.description, COUNT(SD.id) FROM SentimentData as SD JOIN SentimentDescription as SDE ON SDE.id = SD.value WHERE SD.date = ?1 GROUP BY SD.value")
-    List<Object[]> countSentimentByValue(Date date);
+    List<Object[]> countSentimentByValue( LocalDate date);
+
+    @Query("SELECT AVG(value) FROM SentimentData WHERE date = ?1")
+    double calculateAverageSentiment(LocalDate date);
 }
