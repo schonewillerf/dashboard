cb19787d875a3887ad7665c0a8d24d0847353bf6
Jan-Willem van Lunteren <jan-willem.vanlunteren@student.hu.nl>
2021-01-05 11:08:33 +0100

    Cleanup

--- src/main/resources/static/js/recentlyUpdatedIssue.js
+++ src/main/resources/static/js/recentlyUpdatedIssue.js
@@ -39,62 +39,59 @@ httpGetAsync('http://localhost:8080/getUpdatedTasks', 'GET', function (result){
         const parentElemet = document.querySelector(".list-group");
 
         const elementList = document.createElement("li");
-        elementList.classList.add("list-group-item","d-flex","justify-content-between","align-items-center", "container");
+        elementList.classList.add("list-group-item","justify-content-between","align-items-center", "container");
         //create div row
         // add div row
         const elementDivRow=document.createElement('div');
-        elementDivRow.classList.add("row");
+        elementDivRow.classList.add("row", "align-items-center");
         //add div row2
         const elementDivRow2=document.createElement("div");
-        elementDivRow2.classList.add("row");
-        // col-12
+        elementDivRow2.classList.add("row", "align-items-center");
+        // col spacer
+        const divRowspacer=document.createElement("div");
+        divRowspacer.classList.add("col-1");
+        elementDivRow2.appendChild(divRowspacer);
+        // col-11
         const divRow12=document.createElement("div");
-        divRow12.classList.add("col-12");
+        divRow12.classList.add("col-11");
         elementDivRow2.appendChild(divRow12);
+
 
 
          // add col with img
         const elementColImg=document.createElement("div");
-        elementColImg.classList.add("col-2", "contributerImg");
+        elementColImg.classList.add("col-1", "contributerImg");
         elementDivRow.appendChild(elementColImg);
         const elemenAuthorAvatar =document.createElement("img");
         elemenAuthorAvatar.src=obj.avatarUrl;
         elemenAuthorAvatar.title=obj.author;
-        elemenAuthorAvatar.classList.add("w-100");
         elementColImg.appendChild(elemenAuthorAvatar);
+
         // add col with issue summary col 9
         const elementCol9=document.createElement("div");
-        elementCol9.classList.add("col-9", "IssueSummary");
+        elementCol9.classList.add("col-11", "IssueSummary");
         elementDivRow.appendChild(elementCol9);
         const elementPWithSummary=document.createElement("p");
         elementCol9.appendChild(elementPWithSummary);
         const textSummary =document.createTextNode(obj.itemSummary);
         elementPWithSummary.appendChild(textSummary);
-          // sp
 
-
-      // div col-1 for sp
-        const divCol1Sp=document.createElement("div");
-        divCol1Sp.classList.add("col-1");
-
+        // div col-1 for sp
         const elementSpanSP = document.createElement("span");
         elementSpanSP.classList.add("badge","badge-primary","badge-pill");
         const textSP = document.createTextNode(obj.storyPoints);
         elementSpanSP.appendChild(textSP);
-        divCol1Sp.appendChild(elementSpanSP);
-        //elementDivRow.appendChild(divCol1Sp);
 
          // item  status
         const ElementPStatus=document.createElement("p");
         if(obj.itemType === "status") {
-            console.log ("t :"+obj.itemType);
             const iArrow = document.createElement("i");
-            iArrow.classList.add("fas", "fa-angle-double-right", "fa-xs");
-            const changedStatusFrom = obj.changedStatusFrom;
-            const changedStatusTo = obj.changedStatusTo;
+            iArrow.classList.add("fas", "fa-arrow-right", "fa-xs", "ml-2", "mr-2");
+
             const textStatusP = document.createElement("p");
             const textNodeStatusFrom=document.createTextNode(obj.changedStatusFrom);
             const textNodeStatusTo=document.createTextNode(obj.changedStatusTo);
+
             textStatusP.appendChild(textNodeStatusFrom);
             textStatusP.appendChild(iArrow);
             textStatusP.appendChild(textNodeStatusTo);
@@ -107,14 +104,13 @@ httpGetAsync('http://localhost:8080/getUpdatedTasks', 'GET', function (result){
             const textStatusP = document.createElement("p");
             const textStatusItem=document.createTextNode("De omschrijving is gewijzigd.");
             textStatusP.appendChild(textStatusItem);
-
             divRow12.appendChild(textStatusP);
 
         }
 
         elementList.appendChild(elementDivRow);
         elementList.appendChild(elementDivRow2)
-        elementList.appendChild(divCol1Sp);
+        elementList.appendChild(elementSpanSP);
 
         parentElemet.appendChild(elementList);
 
